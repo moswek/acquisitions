136855ce876a374492e220ef3cb5689ef7bdd1ba
Moses <moswekofficial@gmail.com>
2026-03-16 22:49:19 +0300

    implement users route and fix getAllUsers service query

--- src/services/users.services.js
+++ src/services/users.services.js
@@ -2,7 +2,7 @@ import logger from '#config/logger.js';
 import { db } from '#config/database.js';
 import { users } from '#models/user.model.js';
 
-export const getAllUsers = async (req, res) => {
+export const getAllUsers = async () => {
   try {
     return await db
       .select({
@@ -13,7 +13,7 @@ export const getAllUsers = async (req, res) => {
         createdAt: users.createdAt,
         updatedAt: users.updatedAt,
       })
-      .from('users');
+      .from(users);
   } catch (e) {
     logger.error('Error getting users:', e);
     throw e;
